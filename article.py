b30a898cf4bea66dfd9fbe048f54b8b26dcf5fb2
unknown <mrcsptr@gmail.com>
2018-10-31 09:25:32 +0100

    article.py

---
+++ article.py
@@ -0,0 +1,32 @@
+class article:
+
+	def __init__(self, nom, auteur, date, contenu):
+		self.nom = nom
+		self.auteur = auteur
+		self.date = date
+		self.contenu = contenu
+		
+		
+	def setNom(self, nom):
+		self.nom = nom
+		
+	def getNom(self):
+		return self.nom
+		
+	def setAuteur(self, auteur)
+		self.auteur =auteur
+		
+	def getAuteur(self, auteur)
+		return self.auteur
+		
+	def setDate(self, date)
+		self.date = date
+		
+	def getDate(self)
+		return self.date	
+		
+	def setContenu(self, contenu)
+		self.contenu = contenu
+		
+	def getDate(self)
+		return self.contenu	
