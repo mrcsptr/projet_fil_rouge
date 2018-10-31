b1fe4f630c8ab0af5b719085a3c1248ed414d331
unknown <benam.sim@gmail.com>
2018-10-31 12:50:52 +0100

    Ajout page d'accueil (enfin...)

--- index_deco.py
+++ index_deco.py
@@ -3,21 +3,28 @@ from flask import *
 app = Flask (__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    recherche = formulaire_recherche_page(request.form)
-    if request.method == 'POST':
-        return search_results(recherche)
+    return render_template('index.html')
 
-    return render_template('index.html', form=search)
-
-
-"""@app.route("/se_deconnecter")
-def se_deconnecter ():
-    session.pop("e_mail", None)
-    flash('Vous etes maintenant deconnecte.')
-    return redirect(url_for('se_connecter'))"""
 
 
 if __name__ == '__main__' :
     app.run(debug=True)
+
+
+
+
+"""
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return recherche = formulaire_recherche_page(request.form)
+    if request.method == 'POST':
+        return search_results(recherche)
+
+
+@app.route("/se_deconnecter")
+def se_deconnecter ():
+    session.pop("e_mail", None)
+    flash('Vous etes maintenant deconnecte.')
+    return redirect(url_for('se_connecter'))"""
