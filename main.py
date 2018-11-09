a086a7ccf6bf93052fbef156bcf0f576f921e538
unknown <mrcsptr@gmail.com>
2018-11-09 14:00:33 +0100

    modifs commentaires dans main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask import *
 from flask_pymongo import PyMongo
 from article import *
 import datetime
+from commentaire import *
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/wiki"
@@ -26,7 +27,8 @@ def connexion_article(chaine):
 @app.route('/ajouterArticle/', methods=['GET', 'POST'])
 def ajouterArticle():
 	if request.method == 'POST':
-		newArticle = article(request.form["titre_article"],request.form["Auteur_name"], datetime.datetime.now(), request.form["contenu_article"], request.form["categorie_article"], request.form["Mots_cles_article"])
+		newArticle = article(request.form["titre_article"],request.form["Auteur_name"], datetime.datetime.now(), 
+			request.form["contenu_article"], request.form["categorie_article"], request.form["Mots_cles_article"])
 		if newArticle.isValid() == True:
 			ajoutArticle = mongo.db.articles.insert_one(newArticle.format)
 			return render_template('temp_Conf_soumissionArticle.html')
@@ -36,18 +38,19 @@ def ajouterArticle():
 	return render_template('template_FormArticle.html')
 	
 
-@app.route('/article/<chaine>/commentaires/')
-def ajouterCommentaire():
-    newComment = commentaires(chaine, request.form["Auteur_comment"], datetime.datetime.now(),
+@app.route('/<chaine>/commentaires/')
+def ajouterCommentaire(chaine):
+	if request.method == 'POST':
+		newComment = commentaires(chaine, request.form["Auteur_comment"], datetime.datetime.now(),
                               request.form["contenu_commment"])
-    if newComment.isValid() == True:
-        ajoutComment = mongo.db.commentaires.insert_one(newComment.format)
-        return render_template('temp_Conf_soumissionComment.html')  # page à renvoyer
-    else:
-        return render_template(
+		if newComment.isValid() == True:
+			ajoutComment = mongo.db.commentaires.insert_one(newComment.format)
+			return render_template('temp_Conf_soumissionComment.html')  # page à renvoyer
+		else:
+			return render_template(
             'template_FormArticle.html')  # page à renvoyer   / à modifier pour afficher message d'erreur
 
-    return render_template('template_FormCommentaires.html')  # page à consulter pour récupérer les informations nécessaires
+	return render_template('template_FormCommentaires.html')  # page à consulter pour récupérer les informations nécessaires
 
 
 if __name__ == '__main__':
